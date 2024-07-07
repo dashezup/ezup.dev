e9d6181aee4118f9e68aa8644f306ddfc07794bc
Dash Eclipse <dashezup@disroot.org>
2024-07-07 17:02:42 +0000

    feat: deploy.sh: switch to rsync over ssh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-git ls-files .web | tar -czf - -T - | ssh ezup 'tar --no-same-permissions --strip-components=1 -C /var/www/html -xzvf -'
+# git ls-files .web | tar -czf - -T - | ssh ezup 'tar --no-same-permissions --strip-components=1 -C /var/www/html -xzvf -'
+# --human-readable, -h
+# --verbose, -v
+find .web -type f -printf '%P\n' | rsync -hv --rsh=ssh --files-from=- .web ezup:/var/www/html/
